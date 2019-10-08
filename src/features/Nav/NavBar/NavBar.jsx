043e598949185e0e93a4c74a13ee50c7fffe93ae
Nick Massie <45523540+nimas0@users.noreply.github.com>
2019-10-08 10:42:32 -0500

    End of Section 4

--- src/features/Nav/NavBar/NavBar.jsx
+++ src/features/Nav/NavBar/NavBar.jsx
@@ -5,9 +5,9 @@ class NavBar extends Component {
   render() {
     return (
       <Menu inverted fixed='top'>
-        <Container >
+        <Container>
           <Menu.Item header>
-            <img src='assets/logo.png' alt='logo' />
+            <img src='/assets/logo.png' alt='logo' />
             Re-vents
           </Menu.Item>
           <Menu.Item name='Events' />
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 const events = [
   {
@@ -52,33 +53,49 @@ const events = [
       }
     ]
   }
-]
-
+];
 
 class EventDashboard extends Component {
   state = {
     events: events,
     isOpen: false
-
-  }
+  };
 
   handleIsOpenToggle = () => {
-    this.setState(({isOpen}) => ({
+    this.setState(({ isOpen }) => ({
       isOpen: !isOpen
-    }))
-  }
+    }));
+  };
+
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    this.setState(({ events }) => ({
+      events: [...events, newEvent],
+      isOpen: false
+    }));
+  };
 
   render() {
-    const {events, isOpen} = this.state;
+    const { events, isOpen } = this.state;
 
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={events}  />
+          <EventList events={events} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button onClick={this.handleIsOpenToggle} positive content='Create Event' />
-          {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle} />}
+          <Button
+            onClick={this.handleIsOpenToggle}
+            positive
+            content='Create Event'
+          />
+          {isOpen && (
+            <EventForm
+              createEvent={this.handleCreateEvent}
+              cancelFormOpen={this.handleIsOpenToggle}
+            />
+          )}
         </Grid.Column>
       </Grid>
     );
