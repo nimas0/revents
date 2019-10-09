89b3c8c078cd2e60ec6c3b764a21c770cccbe15f
Nick Massie <45523540+nimas0@users.noreply.github.com>
2019-10-09 10:23:07 -0500

    End of Section 6

--- src/features/Nav/NavBar/NavBar.jsx
+++ src/features/Nav/NavBar/NavBar.jsx
@@ -1,32 +1,53 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink, Link, withRouter } from 'react-router-dom';
+import SignedOutMenu from '../Menus/SignedOutMenu';
+import SignedInMenu from '../Menus/SignedInMenu';
 
 class NavBar extends Component {
+  state = {
+    authenticated: true
+  };
+
+  handleSignIn = () => this.setState({ authenticated: true });
+  handleSignOut = () => {
+    this.setState({ authenticated: false });
+    this.props.history.push('/');
+  };
+
   render() {
+    const { authenticated } = this.state;
     return (
       <Menu inverted fixed='top'>
         <Container>
-          <Menu.Item header>
+          {
+            //Link up nav bar with NavLink routes. "Link" is used for button because NavLink talks to semantic UI to highlight active page, do not want button highlighted
+          }
+          <Menu.Item as={NavLink} exact to='/' header>
             <img src='/assets/logo.png' alt='logo' />
             Re-vents
           </Menu.Item>
-          <Menu.Item name='Events' />
+          <Menu.Item as={NavLink} to='/events' name='Events' />
+          <Menu.Item as={NavLink} to='/people' name='People' />
           <Menu.Item>
-            <Button floated='right' positive inverted content='Create Event' />
-          </Menu.Item>
-          <Menu.Item position='right'>
-            <Button basic inverted content='Login' />
             <Button
-              basic
+              as={Link}
+              to='/createEvent'
+              floated='right'
+              positive
               inverted
-              content='Sign Out'
-              style={{ marginLeft: '0.5em' }}
+              content='Create Event'
             />
           </Menu.Item>
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} />
+          )}
         </Container>
       </Menu>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
