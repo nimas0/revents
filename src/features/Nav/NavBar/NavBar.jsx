6a378c64cda705c79629a9ea35453b6186552b29
Nick Massie <45523540+nimas0@users.noreply.github.com>
2019-10-30 09:51:42 -0500

    End of Section 12

--- src/features/Nav/NavBar/NavBar.jsx
+++ src/features/Nav/NavBar/NavBar.jsx
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
-import { openModal } from "../../modals/modalActions";
-import {logout} from '../../auth/authActions'
+import { openModal } from '../../modals/modalActions';
+import { logout } from '../../auth/authActions';
 const actions = {
-  openModal,
-  logout
-}
+ openModal,
+ logout
+};
 
 const mapState = (state) => ({
-  auth: state.auth
-})
+ auth: state.auth
+});
 
 class NavBar extends Component {
-
  handleSignIn = () => this.props.openModal('LoginModal');
  handleSignOut = () => {
   this.props.logout();
@@ -24,8 +23,8 @@ class NavBar extends Component {
  };
 
  handleRegister = () => {
-   this.props.openModal('RegisterModal')
- }
+  this.props.openModal('RegisterModal');
+ };
 
  render() {
   const { auth } = this.props;
@@ -41,22 +40,33 @@ class NavBar extends Component {
       Re-vents
      </Menu.Item>
      <Menu.Item as={NavLink} exact to='/events' name='Events' />
-     <Menu.Item as={NavLink} to='/people' name='People' />
-     <Menu.Item as={NavLink} to='/test' name='test' />
-     <Menu.Item>
-      <Button
-       as={Link}
-       to='/createEvent'
-       floated='right'
-       positive
-       inverted
-       content='Create Event'
-      />
-     </Menu.Item>
+     {// expressions can not return mutliple items or nests so use fragment to fix
+     authenticated && (
+      <Fragment>
+       <Menu.Item as={NavLink} to='/people' name='People' />
+       <Menu.Item as={NavLink} to='/test' name='test' />
+       <Menu.Item>
+        <Button
+         as={Link}
+         to='/createEvent'
+         floated='right'
+         positive
+         inverted
+         content='Create Event'
+        />
+       </Menu.Item>
+      </Fragment>
+     )}
      {authenticated ? (
-      <SignedInMenu signOut={this.handleSignOut} currentUser={auth.currentUser} />
+      <SignedInMenu
+       signOut={this.handleSignOut}
+       currentUser={auth.currentUser}
+      />
      ) : (
-      <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister} />
+      <SignedOutMenu
+       signIn={this.handleSignIn}
+       register={this.handleRegister}
+      />
      )}
     </Container>
    </Menu>
@@ -64,4 +74,9 @@ class NavBar extends Component {
  }
 }
 
-export default withRouter(connect(mapState, actions)(NavBar));
+export default withRouter(
+ connect(
+  mapState,
+  actions
+ )(NavBar)
+);
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -3,37 +3,42 @@ import { Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { createEvent, deleteEvent, updateEvent } from '../eventActions';
 import EventList from '../EventList/EventList';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 const mapState = (state) => ({
-  events: state.events
+ events: state.events,
+ loading: state.async.loading
 });
 
 const actions = {
-  createEvent,
-  deleteEvent,
-  updateEvent
+ createEvent,
+ deleteEvent,
+ updateEvent
 };
 
-
 class EventDashboard extends Component {
+ handleDeleteEvent = (id) => this.props.deleteEvent(id);
 
-  handleDeleteEvent = (id) => 
-    this.props.deleteEvent(id);
-  
-
-  render() {
-    const { events } = this.props;
-    return (
-      <Grid>
-        <Grid.Column width={10}>
-          <EventList events={events} deleteEvent={this.handleDeleteEvent} />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <h2>Activity Feed</h2>
-        </Grid.Column>
-      </Grid>
-    );
+ render() {
+  const { events, loading } = this.props;
+  if (loading) {
+   return <LoadingComponent />;
   }
+
+  return (
+   <Grid>
+    <Grid.Column width={10}>
+     <EventList events={events} deleteEvent={this.handleDeleteEvent} />
+    </Grid.Column>
+    <Grid.Column width={6}>
+     <h2>Activity Feed</h2>
+    </Grid.Column>
+   </Grid>
+  );
+ }
 }
 
-export default connect(mapState,actions)(EventDashboard);
+export default connect(
+ mapState,
+ actions
+)(EventDashboard);
